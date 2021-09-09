7ae8a47a0d4cbcb09456bfc120ec240cbbe7e677
her0ku <46974529+her0ku@users.noreply.github.com>
2021-09-09 18:29:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import gmaps
 import gmaps.datasets
 
 response = requests.get('https://graphhopper.com/api/1/route?point=55.79683470754812, 37.724203498762215&point=55.798432887249085, 37.72355440412516&vehicle=foot&avoid=steps;bridge&points_encoded=false&instructions=true&algorithm=alternative_route'
-                        '&elevation=true&ch.disable=truet&details=road_class&details=road_access&details=road_environment&key=0768f95a-07a6-4a7f-8c7f-117c8ced4b7f')
+                        '&elevation=true&ch.disable=truet&details=road_class&details=road_access&details=road_environment&key=API')
 
 points = []
 road_info = []
@@ -35,7 +35,7 @@ point_cast = fix_points[6]
 point_cast2 = fix_points[7]
 
 response = requests.get(f'https://graphhopper.com/api/1/route?point={point_cast[0]}, {point_cast[1]}&point={point_cast2[0]}, {point_cast[1]}&vehicle=foot&avoid=steps;bridge&points_encoded=false&instructions=true&algorithm=alternative_route'
-                        '&elevation=true&ch.disable=truet&details=road_class&details=road_access&details=road_environment&key=0768f95a-07a6-4a7f-8c7f-117c8ced4b7f')
+                        '&elevation=true&ch.disable=truet&details=road_class&details=road_access&details=road_environment&key=API')
 
 new_points = []
 new_road = []
